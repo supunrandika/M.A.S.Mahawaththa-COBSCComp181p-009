af0301f48b8675b9e562a370938c3dce150e40bd
sahan <sahanjayawardhana0320@gmail.com>
2019-11-21 19:45:30 +0530

    PodFileCommit

---
+++ NIBMArticle/Pods/Target Support Files/Pods-NIBMArticleTests/Pods-NIBMArticleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NIBMArticleTests : NSObject
+@end
+@implementation PodsDummy_Pods_NIBMArticleTests
+@end
---
+++ NIBMArticle/Pods/Target Support Files/Pods-NIBMArticleUITests/Pods-NIBMArticleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NIBMArticleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_NIBMArticleUITests
+@end
