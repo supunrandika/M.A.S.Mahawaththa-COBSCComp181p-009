e309c2525045986bd778c4413b723c0d0098a719
Dev19963 <d0153020@student.nibm.lk>
2019-11-22 23:30:15 +0530

    Face Id

---
+++ NIBMArticle/NIBMArticle/Views/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MenuViewController.swift
+//  NIBMArticle
+//
+//  Created by Isuru Devinda on 11/22/2562 BE.
+//  Copyright © 2562 BE NIBM. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NIBMArticle/NIBMArticle/Views/ProfileViewController.swift
+++ NIBMArticle/NIBMArticle/Views/ProfileViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import LocalAuthentication
 
 class ProfileViewController: UIViewController {
  var window: UIWindow?
@@ -36,6 +37,45 @@ class ProfileViewController: UIViewController {
 //        } catch let signOutError as NSError {
 //            print ("Error signing out: %@", signOutError)
 //        }
+    }
+    
+    @IBAction func GotoMyProfile(_ sender: Any) {
+        
+        let authContext = LAContext()
+        let authReason = "Please use Touch ID to access Your Account"
+        var authError : NSError?
+        
+        if authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &authError){
+            
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: authReason, reply: { (success,error) -> Void in
+                
+                if error != nil{
+                    DispatchQueue.main.async {
+                        self.alert(message: "Authentication Failed")
+                    }
+                    return
+                }
+                
+                
+                if success{
+                    //Go to My Account Page
+                    DispatchQueue.main.async {
+                        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+                        
+                        let initialViewController = storyboard.instantiateViewController(withIdentifier: "MyProfileEdit")
+                        
+                        self.window?.rootViewController = initialViewController
+                        self.window?.makeKeyAndVisible()
+                    }
+                }
+                
+                
+            })
+            
+        }
+        else{
+            self.alert(message: "Authentication Failed")
+        }
     }
     /*
     // MARK: - Navigation
